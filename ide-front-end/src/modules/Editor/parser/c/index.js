e47bd79e4bc02c3be6a0b852b7e6ebe460341a1d
unknown <master@example.com>
2020-02-09 21:51:21 +0900

    IDE 프론트엔드 통합

--- ide-front-end/src/modules/Editor/parser/c/index.js
+++
@@ -1,9 +0,0 @@
-function Parser(src) {
-    const idx = src.indexOf('main');
-    if(idx !== -1) {
-        return [{index: idx, type: "MACRO", token: "main"}]
-    }
-    return [];
-}
-
-export default Parser;
--- ide-front-end/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import openFilesReducer from './open-files';
-
-
-export default combineReducers({
-    openFiles: openFilesReducer
-});
