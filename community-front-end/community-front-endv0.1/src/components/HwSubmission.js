3b2c5aea3fbfb78012acb8c3e5ccbb2eeb379ddd
unknown <master@example.com>
2020-02-09 21:43:28 +0900

    remove previous files

--- community-front-end/community-front-endv0.1/src/components/HwSubmission.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const HwSubmission = (props) => {
-    if(props.state == "submission")
-        return <span className = "submission">제출</span>
-    else
-    return <span className = "not_submission">미제출</span>
-};
-export default HwSubmission;
--- community-front-end/community-front-endv0.1/src/layout/Project/ProjectManage.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-export default class ProjectManage extends Component {
-    render() {
-        return (
-           <>
-           </>
-        )
-    }
-}
